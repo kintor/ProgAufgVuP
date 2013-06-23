1a3634ba342b0c4e7837616e078a8dc2d525f6ad
Torsten Kinzel <torsten.kinzel@innoq.com>
2013-06-23 20:06:08 +0200

    Java-Projekt erstellt

---
+++ MyChordDHT/src/DHT.java
@@ -0,0 +1,7 @@
+
+public class DHT {
+	
+	public static void main(String[] args) {
+	}
+
+}
---
+++ MyChordDHT/src/Node.java
@@ -0,0 +1,8 @@
+
+public class Node {
+
+	private String selfIP;
+	private int selfPort;
+	private String nextIP;
+	private int nextPort;
+}
